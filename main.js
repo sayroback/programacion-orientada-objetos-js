0b4a05d2429f1a5dfc7b1adb2e0666073cb7eb08
Juan Manuel Calderon Robles <sayroback@gmail.com>
2022-06-23 04:13:09 -0500

    Object literals and prototypes in JavaScript

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 console.log("Hola");
+
+const juan = {
+  // Attributes
+  name: "Juan",
+  age: 25,
+  coursesApproved: ["HTML and CSS", "JavaScript"],
+
+  // Methods
+
+  // New course approved
+  approveCourse(newCourse) {
+    this.coursesApproved.push(newCourse);
+  },
+};
+
+// New course approved
+juan.coursesApproved.push("Responsive design");
+console.log(juan);
+
+function Student(name, age, coursesApproved) {
+  this.name = name;
+  this.age = age;
+  this.coursesApproved = coursesApproved;
+}
+
+Student.prototype.approveCourse = function (newCourse) {
+  this.coursesApproved.push(newCourse);
+};
+
+const luis = new Student("Luis", 20, ["Introduction to graphic design"]);
+
+console.log(luis);
